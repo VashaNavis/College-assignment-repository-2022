28ce240d741beaa5e5437f0492b416889912cb19
Sean Anderson <84157570+VashaNavis@users.noreply.github.com>
2023-07-20 00:25:22 -0400

    Add files via upload

---
+++ Myfirstprogram121/Myfirstprogram121.cpp
@@ -0,0 +1,12 @@
+// Myfirstprogram121.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Programmer: Sean Anderson
+// Project number 1
+
+#include <iostream> // This includes the iostream library
+using namespace std; // Specifies the standard namespace for all functions in this project
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    return 0;
+}
